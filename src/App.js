0582434e79f134e7ed71e285d9ed4eab9d1ba2dd
neemchand <neemchand@ucreate.co.in>
2019-07-16 17:04:11 +0530

    dynamic state change for buttons names

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import './App.css';
 import Button from "./components/Button";
 import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+class App extends React.Component  {
+  
+  state = {
+    button1 :'initial state',
+    button2 :'initial state'
+  }
+  
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        button1 :'state changed first',
+        button2 :'state changed second'
+      });
+    },2000);
+  }
+
+  render(){
   return (
     <div className="App">
     <Button name="button1" c_name="btn" />
-    <Button name="button2" c_name="btn-primary" />
-    <Button name="button2" c_name="btn-success" />
+    <Button name={this.state.button1} c_name="btn-primary" />
+    <Button name={this.state.button2} c_name="btn-success" />
     </div>
   );
 }
-
+}
 export default App;
